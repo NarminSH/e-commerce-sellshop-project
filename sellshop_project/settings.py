8d35155dc7d14ef87785a7c5e79dd0df12968492
Narmin <narmin@Narmins-MacBook-Air.local>
2021-08-06 19:29:08 +0400

    changed settings

--- sellshop_project/settings.py
+++ sellshop_project/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sl@)uie6byz3aviy1uvpggv31o-)^d=bb*&je-+k21lrd+i%5g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.environ.get('DEBUG') else True
